9846526fe00c90ebcafce5fb8f0c456dd4b24205
"Aloysius" <"aloycwl@gmail.com">
2022-09-18 09:27:47 +0800

    initialise

--- main.sol
+++ main.sol
@@ -1,6 +1,26 @@
 pragma solidity>0.8.0;//SPDX-License-Identifier:None
+
 contract Main{
-    function test()external pure returns(int a){
-        a=(1+1111)*12322;
+    constructor(){
+
+    }
+    function swap()external{
+        /*
+        Get approval of transferring ETH from msg.sender to this address(this)
+        */
+
+        /*
+        Put into aave and get stETH
+        */
+
+        /*
+        0xdc24316b9ae028f1497c275eb9192a3ea0f67022
+        Exchange from curve.fi and get ETH
+        */
+
+        /*
+        Transfer back to msg.sender the exchanged ETH
+        Deduct admin fee
+        */
     }
 }
