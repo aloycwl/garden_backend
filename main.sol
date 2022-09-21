1c9953c3bddd57b5a26b9ff797ac5ff368202076
"Aloysius" <"aloycwl@gmail.com">
2022-09-21 09:08:55 +0800

    Added Weth

--- main.sol
+++ main.sol
@@ -5,28 +5,21 @@ LendingPool (mainnet): 0x7d2768de32b0b80b7a3454c06bdac94a69ddc7a9
 */
 pragma solidity>0.8.0;//SPDX-License-Identifier:None
 
-interface ILendingPool {
-    //asset to deposit, amount, aToken receiver, referralCode (0 default)
-    function deposit(address,uint,address,uint) external;
+interface IWETHGateway {
+    //amount, lending pool address, aToken receiver, referralCode (0 default)
+    function depositETH(uint,address,address,uint)external;
 }
 
 contract Main{
-    ILendingPool private ilp;
+    IWETHGateway private iwg;
 
     constructor(){
-        ilp=ILendingPool(0x3bd3a20Ac9Ff1dda1D99C0dFCE6D65C4960B3627);
+        iwg=IWETHGateway(0x3bd3a20Ac9Ff1dda1D99C0dFCE6D65C4960B3627);
 
     }
     function swap(uint amt)external payable{
-        /*
-        Get approval of transferring ETH from msg.sender to this address(this)
-        */
-
-        /*
-        Put into aave and get stETH
-        */
-        //0x7d2768de32b0b80b7a3454c06bdac94a69ddc7a9
-        ilp.deposit(address(0),amt,address(this),0);
+        //Put into aave and get aToken
+        iwg.depositETH(amt,0x4bd5643ac6f66a5237E18bfA7d47cF22f1c9F210,address(this),0);
 
         /*
         0xdc24316b9ae028f1497c275eb9192a3ea0f67022
